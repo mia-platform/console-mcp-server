ab66d0a2e181361ed985f961811dd04e7bd95674
demetrio.marino <demetrio.marino@mia-platform.eu>
2025-09-05 17:57:17 +0200

    refactor: more oauthRotuer in separate folder

---
+++ src/server/auth/oauthRouter.ts
@@ -0,0 +1,80 @@
+import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify'
+
+import 'dotenv/config'
+
+const OKTA_CLIENT_ID = process.env.OKTA_CLIENT_ID
+const CONSOLE_URL = process.env.CONSOLE_URL
+
+const getBaseUrlFromRequest = (req: FastifyRequest) => {
+  const { hostname = 'localhost', port = process.env.PORT, protocol = 'http' } = req
+  return `${protocol}://${hostname}:${port}`
+}
+
+export async function oauthRouter (fastify: FastifyInstance) {
+  fastify.get('/.well-known/oauth-protected-resource', async (request: FastifyRequest, reply: FastifyReply) => {
+    const { body, headers } = request
+    const baseUrl = getBaseUrlFromRequest(request)
+
+    fastify.log.debug({
+      message: 'GET /.well-known/oauth-protected-resource called',
+      requestBody: body,
+      requestHeaders: headers,
+    })
+
+    reply.send({
+      resource_name: 'Console MCP Server',
+      resource: `${baseUrl}/mcp`,
+      authorization_servers: [ baseUrl ],
+      scopes_supported: [ 'profile', 'email', 'openid', 'offline-access' ],
+      bearer_methods_supported: [ 'header' ],
+    })
+  })
+
+  fastify.post('/register', async (request: FastifyRequest, reply: FastifyReply) => {
+    const { body, headers } = request
+
+    fastify.log.debug({
+      message: 'POST /register called',
+      requestBody: body,
+      requestHeaders: headers,
+    })
+
+    reply.send({
+      client_id: OKTA_CLIENT_ID,
+    })
+  })
+
+  fastify.get('/.well-known/oauth-authorization-server', async (request: FastifyRequest, reply: FastifyReply) => {
+    const { body, headers } = request
+    const baseUrl = getBaseUrlFromRequest(request)
+
+    fastify.log.debug({
+      message: 'GET /.well-known/oauth-authorization-server called',
+      requestBody: body,
+      requestHeaders: headers,
+    })
+
+    reply.send({
+      issuer: CONSOLE_URL,
+      authorization_endpoint: `${CONSOLE_URL}/api/authorize?appId=console-mcp-server&providerId=okta`,
+      token_endpoint: `${CONSOLE_URL}/api/oauth/token?appId=console-mcp-server&providerId=okta`,
+      scopes_supported: [ 'profile', 'email', 'openid', 'offline-access' ],
+      registration_endpoint: `${baseUrl}/register`,
+      response_types_supported: [
+        'code',
+      ],
+      response_modes_supported: [
+        'query',
+      ],
+      grant_types_supported: [
+        'authorization_code',
+        'refresh_token',
+      ],
+      token_endpoint_auth_methods_supported: [
+        'client_secret_basic',
+        'client_secret_post',
+        'none',
+      ],
+    })
+  })
+}
